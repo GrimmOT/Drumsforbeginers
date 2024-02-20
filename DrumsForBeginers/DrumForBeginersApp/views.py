9871dad431b3bd7675a0b78897f3679b0a3e64e5
GrimmOT <ashutov2009@gmail.com>
2024-02-20 19:38:54 +0300

    New version

--- DrumsForBeginers/DrumForBeginersApp/views.py
+++ DrumsForBeginers/DrumForBeginersApp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
+
+def main_index(request):
+    return redirect("DrumForBeginersApp/")
 
 def index(request):
-    context = {}
+    context = {
+        "title": "Барабаны для всех!"
+    }
     return render(request, 'index.html', context)
-
 
 def all_exercises(request):
     return HttpResponse("Здесь будет переходная станичка на все упражнения!!!")
--- DrumsForBeginers/DrumsForBeginers/urls.py
+++ DrumsForBeginers/DrumsForBeginers/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from DrumForBeginersApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("DrumForBeginersApp/", include("DrumForBeginersApp.urls")),
+    path("", views.main_index)
 ]
